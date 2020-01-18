a46fcf06734214d6353c27e40aab73d7562340d6
huyongpeng <704725075@qq.com>
2020-01-18 14:51:53 +0800

    first commit

---
+++ hahei-server/src/main/java/pers/hututu/server/HaheiServerApplication.java
@@ -0,0 +1,13 @@
+package pers.hututu.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaheiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaheiServerApplication.class, args);
+    }
+
+}
---
+++ hahei-server/src/test/java/pers/hututu/server/HaheiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.hututu.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaheiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
