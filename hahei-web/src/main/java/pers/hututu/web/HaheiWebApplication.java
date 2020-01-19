25520e99ebfd633bd5776896280987824d1bdf60
huyongpeng <704725075@qq.com>
2020-01-19 08:56:28 +0800

    second commit

---
+++ hahei-web/src/main/java/pers/hututu/web/HaheiWebApplication.java
@@ -0,0 +1,13 @@
+package pers.hututu.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaheiWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaheiWebApplication.class, args);
+    }
+
+}
---
+++ hahei-web/src/test/java/pers/hututu/web/HaheiWebApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.hututu.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaheiWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
